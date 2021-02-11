bfa9d623268880924c3949940cc40ca1c642cc2c
b66957am <amir.maula@student.manchester.ac.uk>
2021-02-11 13:07:36 +0000

    Create basic dice functionality

--- diceapp/ViewController.swift
+++ diceapp/ViewController.swift
@@ -12,8 +12,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stepper?.minimumValue = 1
+        stepper?.value = 6
+        outputNumberLabel?.text = String(outputNumberInt)
     }
-
-
+    
+// Outlets
+    @IBOutlet weak var outputNumberLabel:UILabel?
+    @IBOutlet weak var maxNumberLabel:UILabel?
+    @IBOutlet weak var stepper:UIStepper?
+    
+// Internal Variables
+    var outputNumberInt = 0
+    var maxNumberInt = 6
+    
+    @IBAction func onReroll(){
+        outputNumberInt = Int.random(in: 1..<maxNumberInt+1)
+        print(String(outputNumberInt))
+        outputNumberLabel?.text = String(outputNumberInt)
+    }
+    
+    @IBAction func stepperValueChanges(_ sender:UIStepper){
+        print("Stepper Value: \(sender.value)")
+        maxNumberInt = Int(sender.value)
+        print(String(maxNumberInt))
+        maxNumberLabel?.text = String(maxNumberInt)
+    }
 }
 
